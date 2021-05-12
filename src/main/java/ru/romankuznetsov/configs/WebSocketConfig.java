ec0c56bcb7e84a92603feaf459f30a44a5c7c85f
RomanK <romulus_666@mail.ru>
2021-05-12 14:34:51 +0600

    DZ-3 merge

---
+++ src/main/java/ru/romankuznetsov/configs/WebSocketConfig.java
@@ -0,0 +1,8 @@
+package ru.romankuznetsov.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+@Configuration
+public class WebSocketConfig {
+}
